aed62ef354797b93d0a5f0a9b8cda479bd6b2da0
DhruvrajsinhVaghela <dhruvrajsinh.vaghela@etatvasoft.com>
2024-01-18 18:23:02 +0530

    patient submit request screen completed

--- js/patient_site.js
+++ js/patient_site.js
@@ -3,7 +3,6 @@ let light = 1;
 function myFunction() {
     if (light == 1) {
         document.body.style.backgroundColor = "black";
-        document.getElementById("heading").style.color = "#08BCEB";
         document.getElementById("terms").style.color = "#08BCEB";
         document.getElementById("darkmode-btn").style.backgroundColor = "black";
         document.getElementById("vr1").style.color = "08BCEB";
@@ -11,30 +10,10 @@ function myFunction() {
     }
     else {
         document.body.style.backgroundColor = "white";
-        document.getElementById("heading").style.color = "black";
         document.getElementById("terms").style.color = "black";
         document.getElementById("darkmode-btn").style.backgroundColor = "white";
         document.getElementById("vr1").style.color = "black";
         light = 1;
     }
 
-}
-
-
-let size = 0;
-function showhide() {
-
-    icon = document.getElementById("eye");
-    pw = document.getElementById("floatingPassword");
-    if (size == 0) {
-        pw.type = "text";
-        icon.src = "/images/eye-slash.svg";
-        size = 1;
-    }
-    else {
-        pw.type = "password";
-        icon.src = "/images/eye.svg";
-        size = 0;
-
-    }
 }
